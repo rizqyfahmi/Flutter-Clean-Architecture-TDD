a14d927a1acf80e3cb5f4f43d83017c0100df9c5
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-19 20:40:44 +0700

    Resolve the test we built by create code for GetRandomNumberTrivia usecase

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/usecases/usecase.dart';
+import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class GetRandomNumberTrivia implements Usecase<NumberTrivia, NoParam> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParam noParam) {
+    return repository.getRandomNumberTrivia();
+  }
+  
+}
+
+class NoParam extends Equatable{
+  @override
+  List<Object?> get props => [];
+  
+}
