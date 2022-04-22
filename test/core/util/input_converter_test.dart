4bac204c88d960a20d546633d9f0b34f3686cfe3
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-22 08:46:29 +0700

    Test: InputConverter

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group("stringToUnsignedInteger", () {
+    test("Should return an integer when the string represents an unsigned integer", () {
+      // arrange
+      const str = "123";
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, const Right(123));
+    });
+
+    test("Should return a failure when the string is not an integer", () {
+      // arrange
+      const str = "abc";
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+
+    test("Should return a failure when the string is a negative integer", () {
+      // arrange
+      const str = "-123";
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
