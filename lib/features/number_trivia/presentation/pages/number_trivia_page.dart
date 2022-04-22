a0430f5588411f2e9f3d0342704ea31d0d19d7ec
rizqyfahmi <rizqyfahmi@yahoo.com>
2022-04-22 12:06:02 +0700

    Build NumberTriviaPage with its components

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/loading_widget.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/message_display.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/trivia_controls.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/trivia_display.dart';
+import 'package:number_trivia/injection_container.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Number Trivia'),
+      ),
+      body: buildBody(context),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return MessageDisplay(message: 'Start searching');
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  } else if (state is Error) {
+                    return MessageDisplay(message: state.message);
+                  }
+                  return Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Placeholder(),
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              TriviaControls(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
